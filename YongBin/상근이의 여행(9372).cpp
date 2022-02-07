d92cab00d46154eef169031c74cefbbf2e7c1c65
Choyongbin <x7520@naver.com>
2022-02-07 10:16:39 +0900

    BOJ_9372
    MST의 간선 == n-1

---
+++ YongBin/상근이의 여행(9372).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		vector<vector<int>> arr(1001) ;
+		int n, m, a, b;
+
+		cin >> n >> m;
+		for (int j = 0; j < m; j++) {
+			cin >> a >> b;
+		}
+		cout << n - 1 <<"\n";
+		
+
+	}
+	return 0;
+}
