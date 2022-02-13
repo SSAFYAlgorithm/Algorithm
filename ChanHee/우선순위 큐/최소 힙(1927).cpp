285ec2ebdc1b3849073f8df9df94579d60b8cbb9
chani97 <80206202+chani97@users.noreply.github.com>
2022-02-13 19:46:36 +0900

    최소 힙(1927번)

---
+++ ChanHee/우선순위 큐/최소 힙(1927).cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+
+int main() {
+	int a;
+	priority_queue<int> q;
+	cin >> a;
+	int i = 0;
+	while(i < a) {
+		int b;
+		cin >> b;
+		b = b * -1;
+		if (b == 0) {
+			if (q.empty()) cout << '0' << '\n';
+
+			else {
+				cout << q.top() * -1 << '\n';
+				q.pop();
+			}
+		}
+		else {
+			q.push(b);
+		}
+		i++;
+	}
+	return 0;
+}
