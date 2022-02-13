ea13f491c28613e71ececc77d14540f4403bdeee
Hany-Kim <hanykim1210@gamil.com>
2022-02-14 06:11:43 +0900

    백준_우선순위큐_v1

---
+++ JuHan/백준_우선순위큐/최대 힙/소스.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	int time;
+	cin >> time;
+	int i, x;
+	priority_queue<int>q;
+	for (i = 0; i < time; i++) {
+		cin >> x;
+		// x=0이면 최대값 출력하고 제거
+		if (x == 0) {
+			// 큐 비어있으면
+			if (q.empty()) cout << 0 << '\n';
+			// 안 비어있으면
+			else {
+				cout << q.top() << '\n';
+				q.pop();
+			}
+		}
+		// x!=0이면 x삽입
+		else q.push(x);
+	}
+	return 0;
+}
