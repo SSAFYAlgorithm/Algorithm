81f7ad1ba1faa732b6e1c5dc5fb472e9d825f798
choisang07 <sangbum96@naver.com>
2022-02-16 22:42:19 +0900

    푸리중

--- SangBeom/DFS와 BFS/7562 나이트의 이동.cpp
+++ SangBeom/DFS와 BFS/7562 나이트의 이동.cpp
@@ -3,47 +3,50 @@
 using namespace std;
 
 int arr[301][301] = { 0 };
-int cnt[301][301] = { 0 };
 int N;
-bool check[301][301] = { 0 };
+int startX, startY;
+int endX, endY;
+int flag = 0;
 void bfs(int y, int x) {
     queue<pair<int, int>>que;
     que.push(pair<int, int>(y, x));
-    check[y][x] = 1;
-    cnt[y][x] = 1;
+    arr[y][x] = 1;
     while (1) {
-
+        que.pop();
         int dx[8] = { -2,-1,1,2,-2,-1,1,2 };
         int dy[8] = { -1,-2,-2,-1,1,2,2,1 };
         for (int i = 0;i < 8;i++) {
             int tempX = x + dx[i];
             int tempY = y + dy[i];
             if (!(tempX < 0 || tempY < 0 || tempX >= N || tempY >= N)) {
-                if (cnt[tempX][tempY]) {
-                    cnt[y][x] = cnt[tempX][tempY] + 1;
+                if (arr[tempX][tempY]) {
+                    arr[y][x] = arr[tempX][tempY] + 1;
                     break;
                 }
             }
         }
-        que.pop();
+
         for (int i = 0;i < 8;i++) {
             int tempX = x + dx[i];
             int tempY = y + dy[i];
             if (!(tempX < 0 || tempY < 0 || tempX >= N || tempY >= N)) {
-                if (!check[tempY][tempX]) {
+                if (!arr[tempY][tempX]) {
                     que.push(pair<int, int>(tempY, tempX));
-                    check[y][x] = true;
+
                 }
             }
         }
         if (que.empty()) {
+            flag = 1;
             break;
         }
         y = que.front().first;
         x = que.front().second;
-
+        if (y == endY && x == endX) {
+            break;
+        }
     }
-
+    while (!que.empty()) que.pop();
 }
 int main()
 {
@@ -51,13 +54,23 @@ int main()
     cin >> testCase;
     for (int i = 0;i < testCase;i++) {
         cin >> N;
-        int startX, startY;
+
         cin >> startX >> startY;
-        int endX, endY;
         cin >> endX >> endY;
-        arr[startY][startX] = 1;   //시작점
-        arr[endY][endX] = 100;      //종점
         bfs(startY, startX);
-        cout << cnt[endY][endX] << '\n';
+        if (flag) {
+            cout << "0\n";
+        }
+        else {
+            cout << arr[startY][startX] << '\n';
+        }
+        flag = 0;
+        for (int k = 0;k < N;k++) {
+            for (int j = 0;j < N;j++) {
+                arr[k][j] = 0;
+            }
+        }
     }
-}
+}\
+
+
