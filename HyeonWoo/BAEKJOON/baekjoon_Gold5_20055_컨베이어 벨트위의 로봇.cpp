e1147296f46ae46cac69356c5e6b2c3300707223
YuHyeonwoo <97938271+YuHyeonwoo@users.noreply.github.com>
2022-04-14 14:02:22 +0900

    20220413

---
+++ HyeonWoo/BAEKJOON/baekjoon_Gold5_20055_컨베이어 벨트위의 로봇.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int N, K;
+int Life[220];
+int Robot[220]; // 1이면 로봇있는상태
+int level = 1;
+int Cnt; // 0의개수
+void Turn() {
+	int temp = Life[2*N];
+	int RobotTmp = Robot[2*N];
+	for (int i = 2*N; i >= 2; i--) {
+		Life[i] = Life[i - 1];
+		Robot[i] = Robot[i - 1];
+	}
+	Life[1] = temp;
+	Robot[1] = RobotTmp;
+}
+
+// 로봇이동
+void Move() {
+	for (int i = N-1; i >= 1; i--) {
+		// 만약 움직이려하는 곳의 수명이 1이상이고, 해당칸에 로봇이 없다면 이동
+		if (Robot[i] == 1) {
+			if (Life[i + 1] > 0 && Robot[i + 1] == 0) {
+				Robot[i + 1] = 1;
+				Robot[i] = 0;
+				Life[i + 1]--;
+				// 만약 수명이 0에 도달하면 0카운트 증가
+				if (Life[i + 1] == 0) {
+					Cnt++;
+				}
+				// 만약 Cnt가 K라면 바로 return 후 정답출력할예정
+				if (Cnt == K) {
+					return;
+				}
+			}
+		}
+	}
+	// 만약 Robot[N] == 1이면 탈출
+	if (Robot[N] == 1) {
+		Robot[N] = 0;
+	}
+}
+
+int main() {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen_s(new FILE*, "input.txt", "r", stdin);
+	cin >> N >> K;
+	for (int i = 1; i <= 2*N; i++) {
+		cin >> Life[i];
+	}
+
+	while (1) {
+		// 1. 회전
+		Turn();
+		// 회전 후 로봇이 N번에있다면 Robot 탈출;
+		if (Robot[N] == 1) {
+			Robot[N] = 0;
+		}
+
+		// 2. 로봇이동
+		Move();
+		if (Cnt >= K) {
+			break;
+		}
+
+		// 3. 로봇올리기
+		if (Life[1] > 0) {
+			Robot[1] = 1;
+			Life[1]--;
+			// 또, 0에도달하면 개수센다
+			if (Life[1] == 0) {
+				Cnt++;
+				// Cnt가 늘어났으니 K랑 비교 같거나 크면 종료
+				if (Cnt >= K) {
+					break;
+				}
+			}
+		}
+
+		//모든작업이 끝나면 level 늘리고 반복
+		level++;
+
+	}
+
+	cout << level;
+	return 0;
+}
---
+++ HyeonWoo/BAEKJOON/baekjoon_Gold5_2011_암호코드.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <vector>
+using namespace std;
+
+string arr;
+int dp[5010];
+// dp 알고리즘
+int main() {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen_s(new FILE*, "input.txt", "r", stdin);
+	
+	cin >> arr;
+	// 첫번째는 무조건 1개 (0이면 0) , 0이면 무조건 불가능
+	if (arr[0] == '0') {
+		cout << 0;
+		return 0;
+	}
+	else {
+		dp[1] = 1;
+	}
+
+	// 두번째 경우도 미리 초기화
+	if (arr.size() >= 2) {
+		int two = arr[1] - '0';
+		int one = arr[0] - '0';
+		int num12 = one * 10 + two;
+		if (two == 0) {
+			if (num12 > 26) {
+				cout << 0;
+				return 0;
+			}
+			else {
+				dp[2] = 1;
+			}
+		}
+		else {
+			if (num12 > 26) {
+				dp[2] = 1;
+			}
+			else {
+				dp[2] = 2;
+			}
+		}
+
+		// 3번부터 for문
+		for (int i = 3; i <= arr.size(); i++) {
+			int now = arr[i - 1] - '0';
+			int last = arr[i - 2] - '0';
+			int num = last * 10 + now;
+			if (now == 0) {
+				if (num <= 26 && num>0) {
+					// 0이오는경우는 i-2 경우에 십의자리 하나를 붙힌것과 같기 때문에
+					dp[i] = dp[i - 2];
+				}
+			}
+			else {
+				// last가 0인경우도 존재 이경우 직전의 경우에 1의자리 하나만 붙힌것만 인정
+				if (last == 0) {
+					dp[i] = dp[i - 1];
+				}
+				// last와 now가 둘다 0이 아닌 경우
+				else {
+					if (num <= 26) {
+						dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000;
+					}
+					else {
+						dp[i] = dp[i - 1];
+					}
+				}
+			}
+		}
+	}
+
+	cout << dp[arr.size()];
+	
+	
+	return 0;
+}
