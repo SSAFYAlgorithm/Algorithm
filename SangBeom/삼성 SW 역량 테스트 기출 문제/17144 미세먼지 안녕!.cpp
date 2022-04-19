32d48a3537d3e93dfc83b63ed2426ded89098880
choisang07 <sangbum96@naver.com>
2022-04-19 22:46:07 +0900

    0419

--- SangBeom/삼성 SW 역량 테스트 기출 문제/17144 미세먼지 안녕!.cpp
+++ SangBeom/삼성 SW 역량 테스트 기출 문제/17144 미세먼지 안녕!.cpp
@@ -14,16 +14,16 @@ void dust()
 	for (int i = 0;i < R;i++) {
 		for (int j = 0;j < C;j++) {
 			if (MAP[i][j] > 0) {
-				int sum = 0;
+				int cnt = 0;
 				for (int k = 0;k < 4;k++) {
 					int tr = dr[k] + i;
 					int tc = dc[k] + j;
 					if (tr < 0 || tc < 0 || tr >= R || tc >= C) continue;
 					if (MAP[tr][tc] == -1) continue;
 					temp[tr][tc] += MAP[i][j] / 5;
-					sum += MAP[i][j] / 5;
+					cnt += 1;
 				}
-				temp[i][j] += (MAP[i][j] - sum);
+				temp[i][j] += (MAP[i][j] - (MAP[i][j] / 5 * cnt));
 			}
 		}
 	}
@@ -33,7 +33,7 @@ void dust()
 }
 void clean()
 {
-	int nowX = r1;
+	/*int nowX = r1;
 	int nowY = 0;
 	int sr[4] = { -1,0,1,0 };
 	int sc[4] = { 0,1,0,-1 };
@@ -50,6 +50,8 @@ void clean()
 			MAP[sx - sr[i]][sy - sc[i]] = MAP[sx][sy];
 		}
 	}
+	MAP[r1][0] = -1;
+	MAP[r1][1] = 0;
 	nowX = r2;
 	nowY = 0;
 	int er[4] = { 1,0,-1,0 };
@@ -67,15 +69,35 @@ void clean()
 			MAP[sx - er[i]][sy - ec[i]] = MAP[sx][sy];
 		}
 	}
-	MAP[r1][0] = -1;
+
 	MAP[r2][0] = -1;
+	MAP[r2][1] = 0;*/
+	// 위의 공기 순환 (반시계)
+	for (int i = r1 - 1; i > 0; i--)
+		MAP[i][0] = MAP[i - 1][0];
+	for (int i = 0; i < C - 1; i++)
+		MAP[0][i] = MAP[0][i + 1];
+	for (int i = 1; i <= r1; i++)
+		MAP[i - 1][C - 1] = MAP[i][C - 1];
+	for (int i = C - 1; i > 1; i--)
+		MAP[r1][i] = MAP[r1][i - 1];
 	MAP[r1][1] = 0;
+
+	for (int i = r2 + 1; i < R - 1; i++)
+		MAP[i][0] = MAP[i + 1][0];
+	for (int i = 0; i < C - 1; i++)
+		MAP[R - 1][i] = MAP[R - 1][i + 1];
+	for (int i = R - 1; i >= r2; i--)
+		MAP[i][C - 1] = MAP[i - 1][C - 1];
+	for (int i = C - 1; i > 1; i--)
+		MAP[r2][i] = MAP[r2][i - 1];
 	MAP[r2][1] = 0;
 }
 int solve()
 {
 	for (int i = 0; i < T;i++) {
 		dust();
+
 		clean();
 	}
 	int sum = 0;
@@ -90,7 +112,7 @@ int solve()
 }
 int main()
 {
-	//freopen("input.txt", "r", stdin);
+	freopen("input.txt", "r", stdin);
 	cin >> R >> C >> T;
 	int flag = 1;
 	for (int i = 0;i < R;i++) {
